93d7c2508696d883189d8df6d1b2cfc7fa70a280
Shawn Guo <sguo37@jhu.edu>
2024-06-07 09:48:44 -0400

    6/7/24
    -issue discovered: scene transition needs decoupling from current scene

--- SystemDemo/Assets/EventArch/TransitionData.cs
+++ SystemDemo/Assets/EventArch/TransitionData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: DECOUPLE FROM CURRENT SCENE
 [CreateAssetMenu(fileName = "TransitionData", menuName = "Scene Management/Transition Data")]
 public class TransitionData : ScriptableObject {
     public string sceneToLoad;
