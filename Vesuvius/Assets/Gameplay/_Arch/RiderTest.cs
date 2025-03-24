8bd0c424a4e8d0b0dcfeeb30158b05e0801a34db
Shawn Guo <sguo37@jhu.edu>
2025-03-24 12:55:59 -0400

    Rider Test

--- Vesuvius/Assets/Gameplay/_Arch/RiderTest.cs
+++ Vesuvius/Assets/Gameplay/_Arch/RiderTest.cs
@@ -3,8 +3,33 @@ using UnityEngine;
 
 public class RiderTest : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] int number;
+    void Start()
     {
-        Debug.Log("Hello World!");
+        PrintNumbersInRangeInclusive(3, 7);
+    }
+
+    /// <summary>
+    /// Prints all numbers inclusively within the specified range, starting from numA to numB.
+    /// </summary>
+    /// <param name="numA">The starting number of the range.</param>
+    /// <param name="numB">The ending number of the range.</param>
+    void PrintNumbersInRangeInclusive(int numA, int numB) 
+    {
+        for (int i = numA; i <= numB; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    /// <summary>
+    /// Multiplies two float values and returns the result.
+    /// </summary>
+    /// <param name="a">The first float value to multiply.</param>
+    /// <param name="b">The second float value to multiply.</param>
+    /// <returns>The product of the two float values.</returns>
+    float MultiplyTwoValues(float a, float b)
+    {
+        return a * b;
     }
 }
