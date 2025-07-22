e75b9c630877c3a6f9fee0b9214223dfb9e3788c
Brett Wolfinger <bdwolfinger@comcast.net>
2025-07-22 13:19:43 -0400

    Step 1 in overhauling the controls text replacement system. Got it working on the player tutorials, going to get it working on all interactables. Now you define your text in the text box on the canvas of your object and put actions in {} to replace them. Would be smart to make it a required component of interactables at some point.

---
+++ Vesuvius/Assets/_Gameplay/_Arch/ControlsTextReplacement.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Text.RegularExpressions;
+using _Gameplay._Arch;
+using Events._Arch;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(GameEventListener))] //call OnControlsChanged with ControlsChanged Game Event
+public class ControlsTextReplacement : MonoBehaviour
+{
+    public TextMeshProUGUI[] textsNeedingReplacement;
+    private string[] textTemplates; //caches text before replacement so we can reuse it
+    private string pattern = "{((.|\n|\r)*)}"; //text to be replaced must be enclosed in {}
+
+    private PlayerInput m_PlayerInput;
+
+    // Instantiate the regular expression object.
+    private Regex r;
+
+    public void OnControlsChanged()
+    {
+        for (int i = 0; i < textsNeedingReplacement.Length; i++)
+        {
+            MatchCollection matches = r.Matches(textTemplates[i]);
+            if (matches.Count == 0)
+            {
+                Debug.LogError("No text replacement found, is your action enclosed in {}?" + "\n"
+                    + "Check " + GetFullName(textsNeedingReplacement[i].gameObject));
+            }
+            //doesn't currently handle having multiple text replacements required in one line
+            foreach (Match match in matches)
+            {
+                //protect against key not found exception in the case of mistakes and prints trace to text where issue occurs
+                try
+                {
+                    textsNeedingReplacement[i].text = textTemplates[i].Replace("{" + match.Groups[1].Value + "}",
+                        GetBindingDisplayStringOrCompositeName(m_PlayerInput.actions[match.Groups[1].Value]));
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e.Message + "\n" + "Check " + GetFullName(textsNeedingReplacement[i].gameObject));
+                }
+            }
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        r = new Regex(pattern);
+        textTemplates = new string[textsNeedingReplacement.Length];
+        for (int i = 0; i < textsNeedingReplacement.Length; i++)
+        {
+            textTemplates[i] = textsNeedingReplacement[i].text;
+        }
+
+        m_PlayerInput = FindFirstObjectByType<PlayerSpawner>()?.GetRuntimePlayer().GetComponent<PlayerInput>();
+
+        OnControlsChanged();
+    }
+
+    string GetBindingDisplayStringOrCompositeName(InputAction action)
+    {
+        // if composite action / can't use binding index or need to specify
+        int bindingIndex = action.GetBindingIndex(group: m_PlayerInput.currentControlScheme);
+
+        if (action.bindings[bindingIndex].isPartOfComposite)
+        {
+            // hard coded logic - assumes that if you found a part of a composite, that it's the first one.
+            // And that the one preceeding it, must be the 'Composite head' that contains the parts
+            return action.bindings[bindingIndex - 1].name;
+        }
+        else { return action.GetBindingDisplayString(); } // if not a composite, bindingId can just be updated
+    }
+    string GetFullName(GameObject go)
+    {
+        string name = go.name;
+        while (go.transform.parent != null)
+        {
+
+            go = go.transform.parent.gameObject;
+            name = go.name + "/" + name;
+        }
+        return name;
+    }
+}
