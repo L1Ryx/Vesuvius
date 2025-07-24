206389b94a3997db72005e2c1a06280fd5dbf078
Brett Wolfinger <bdwolfinger@comcast.net>
2025-07-24 16:33:38 -0400

    Learning how to use tiled and aseprite to integrate them into our workflow

--- Vesuvius/Assets/Art/Tilesets/Snow-Tunnel-Tileset.tsx
+++ Vesuvius/Assets/Art/Tilesets/Snow-Tunnel-Tileset.tsx
@@ -1,4 +1,31 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Snow-Tunnel-Tileset" tilewidth="16" tileheight="16" tilecount="400" columns="20">
  <image source="Snow-Tunnel-Tileset.png" width="320" height="320"/>
+ <tile id="241" probability="0.25"/>
+ <tile id="242" probability="0.25"/>
+ <tile id="261" probability="0.25"/>
+ <tile id="262" probability="0.25"/>
+ <wangsets>
+  <wangset name="terrain" type="corner" tile="-1">
+   <wangcolor name="frost" color="#ff0000" tile="103" probability="1"/>
+   <wangcolor name="inner" color="#00ff00" tile="23" probability="1"/>
+   <wangtile tileid="2" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="3" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="6" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="7" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="8" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="22" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="23" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="24" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="26" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="28" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="42" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="43" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="44" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="46" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="47" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="48" wangid="0,2,0,2,0,2,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
