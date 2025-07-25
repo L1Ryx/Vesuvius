d78d232fca5c4ed5f48876be43886b8752cd5120
Brett Wolfinger <bdwolfinger@comcast.net>
2025-07-24 23:18:47 -0400

    Made good progress on tiled maps, saving work before trying to do a little project folder reorg

--- Vesuvius/Assets/Art/Tilesets/Snow-Tunnel-Tileset.tsx
+++ Vesuvius/Assets/Art/Tilesets/Snow-Tunnel-Tileset.tsx
@@ -1,6 +1,151 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Snow-Tunnel-Tileset" tilewidth="16" tileheight="16" tilecount="400" columns="20">
  <image source="Snow-Tunnel-Tileset.png" width="320" height="320"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="43">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="140">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="141">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="143">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="144">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="160">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="164">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="241" probability="0.25"/>
  <tile id="242" probability="0.25"/>
  <tile id="261" probability="0.25"/>
@@ -26,6 +171,34 @@
    <wangtile tileid="46" wangid="0,1,0,2,0,2,0,2"/>
    <wangtile tileid="47" wangid="0,1,0,2,0,2,0,1"/>
    <wangtile tileid="48" wangid="0,2,0,2,0,2,0,1"/>
+  </wangset>
+  <wangset name="backgrounds" type="corner" tile="-1">
+   <wangcolor name="edges" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="inner" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="220" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="221" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="222" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="223" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="240" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="241" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="242" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="243" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="260" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="261" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="262" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="263" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="280" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="281" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="282" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="283" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="300" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="301" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="302" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="320" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="322" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="340" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="341" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="342" wangid="0,2,0,2,0,2,0,1"/>
   </wangset>
  </wangsets>
 </tileset>
---
+++ Vesuvius/Assets/Art/Tilesets/ice bridge.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ice bridge" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="Ice-Bridge-Tileset.png" width="128" height="128"/>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+</tileset>
