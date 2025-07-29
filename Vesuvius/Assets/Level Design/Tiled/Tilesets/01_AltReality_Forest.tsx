4e5c455790eaf10bffafd0690117a60d6291aa3d
bwolfin1 <bwolfin1@jhu.edu>
2025-07-29 15:21:28 -0400

    tested creating alt reality maps in tiled - pretty easy you can just replace the tilesets. It might be a little annoying if we are constantly making changes but we can look for stronger solutions then.

---
+++ Vesuvius/Assets/Level Design/Tiled/Tilesets/01_AltReality_Forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="01_AltReality_Forest" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="../../Tilesets/Alt-Tunnel-Tileset.png" width="320" height="320"/>
+</tileset>
